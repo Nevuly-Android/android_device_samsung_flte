c505feb583c849331e343352961600c518669b05
hiru <rlawoehd187@naver.com>
2023-03-19 22:20:14 +0900

    Revert "flte: Set SELinux enforcing"
    This reverts commit 91548e838c7c8e5b61089611024ca904c9b4a9ce.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -33,7 +33,7 @@ TARGET_FS_CONFIG_GEN += $(DEVICE_PATH)/config.fs
 
 # Kernel
 BOARD_KERNEL_BASE := 0x00000000
-BOARD_KERNEL_CMDLINE := console=null androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3
+BOARD_KERNEL_CMDLINE := console=null androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3 androidboot.selinux=permissive
 BOARD_KERNEL_IMAGE_NAME := zImage
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_SEPARATED_DT := true
